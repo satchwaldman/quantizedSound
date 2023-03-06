d98d6df6b4af65bb52c203237c13314f053f4c0d
satchwaldman <satchwaldman@gmail.com>
2023-03-06 12:45:35 -0500

    added in the end

--- soundQuantizer.py
+++ soundQuantizer.py
@@ -3,14 +3,14 @@ import numpy as np
 import soundfile as sf
 
 # Load the audio file
-filename = "audio_data/ring_of_fire_audio.mp3"
+filename = "audio_data/in_the_end_audio.mp3"
 audio, sr = librosa.load(filename, sr=None)
 
 # Convert the audio signal to a numerical representation
 audio_int = np.int16(audio * 32767) # Convert to 16-bit integers
 
 # Define the number of bits to use for quantization
-num_bits = 1
+num_bits = 2
 
 # Calculate the range of values that can be represented by the chosen number of bits
 quantization_range = 2**(num_bits - 1)
@@ -22,4 +22,4 @@ quantized_audio = np.round(audio_int / (32767/quantization_range))
 quantized_audio_int = np.int16(quantized_audio * (32767/quantization_range))
 
 # Save the quantized audio file using soundfile
-sf.write("ring_of_fire_outputs/ring_of_fire_1bit.wav", quantized_audio_int, sr, subtype='PCM_16')
+sf.write("in_the_end_outs/in_the_end_2bit.wav", quantized_audio_int, sr, subtype='PCM_16')
